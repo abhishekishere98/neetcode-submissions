118c44860f50161bcfdff96b810795f162c2f904
abhishekishere98 <84777190+abhishekishere98@users.noreply.github.com>
2026-06-03 11:18:40 +0530

    Add: top-k-elements-in-list - submission-24

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-24.py
@@ -0,0 +1,55 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # map = {}
+        # for n in nums:
+        #     if n in map:
+        #         map[n] += 1;
+        #     else:
+        #         map[n] = 1
+        # result = sorted(map.items(),key=lambda item: item[1])
+        # output = []
+        # for i in range(len(result)-k, len(result)):
+        #     output.append(result[i][0])
+        # return output
+
+        # # Frequency array for numbers with frequency
+        # freq = [[] for i in range(len(nums)+1)]
+
+        # # Get frequency of each element
+        # count = {}
+        # for n in nums:
+        #     count[n] = 1 + count.get(n,0)
+        # # initialize frequency array
+        # for n,c in count.items():
+        #     freq[c].append(n)
+        # # Get K most frequent and save to output array 
+        # output = []
+        # for i in range (len(freq)-1,0,-1):
+        #     for n in freq[i]:
+        #         output.append(n)
+        #         if len(output) == k:
+        #             return output
+
+        # Get count of each element 
+        count ={}
+        for n in nums:
+            count[n] = 1+count.get(n,0)
+        
+        # Frequency list to store nums
+        freq = [[] for i in range(len(nums)+1)]
+
+        # Intialize frequency list:
+        for n,c in count.items(): 
+            freq[c].append(n)
+        
+        # Get k most frequent from frequency list.
+        output = []
+        for i in range (len(freq)-1, -1, -1):
+            for j in freq[i]:
+                output.append(j)
+                if len(output) == k:
+                    return output
+            
+        
+        
+
